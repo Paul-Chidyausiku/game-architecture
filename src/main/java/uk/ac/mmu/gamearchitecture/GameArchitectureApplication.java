6bb59aaa9f0c60917f898271a6f3e40bbe3cd5e6
Paul Chidyausiku <paulchidyausiku@Pauls-Air.lan>
2026-05-15 18:17:03 +0100

    Initial commit

---
+++ src/main/java/uk/ac/mmu/gamearchitecture/GameArchitectureApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.mmu.gamearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/ac/mmu/gamearchitecture/GameArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.mmu.gamearchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
